f1919c3dcde27301c0dcc746c3391c757713140b
James Carbine <jamescarbine@gmail.com>
2017-11-14 11:33:09 -0700

    Add context_id and oauth_consumer_key to middleware

--- libs/middleware/api.js
+++ libs/middleware/api.js
@@ -4,6 +4,11 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = undefined;
+
+var _extends2 = require('babel-runtime/helpers/extends');
+
+var _extends3 = _interopRequireDefault(_extends2);
+
 exports.apiRequest = apiRequest;
 
 var _api = require('../api/api');
@@ -16,7 +21,13 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function apiRequest(store, action) {
   var state = store.getState();
-  var promise = _api2.default.execRequest(action.method, action.url, state.settings.api_url, state.jwt, state.settings.csrf_token, action.params, action.body, action.headers, action.timeout);
+  var updatedParams = (0, _extends3.default)({
+    // Add the context id from the lti launch
+    context_id: state.settings.context_id,
+    // Add consumer key to requests to indicate which lti app requests are originating from.
+    oauth_consumer_key: state.settings.oauth_consumer_key
+  }, action.params);
+  var promise = _api2.default.execRequest(action.method, action.url, state.settings.api_url, state.jwt, state.settings.csrf_token, updatedParams, action.body, action.headers, action.timeout);
 
   if (promise) {
     promise.then(function (response) {
--- src/middleware/api.js
+++ src/middleware/api.js
@@ -3,13 +3,20 @@ import { DONE } from '../constants/wrapper';
 
 export function apiRequest(store, action) {
   const state = store.getState();
+  const updatedParams = {
+    // Add the context id from the lti launch
+    context_id: state.settings.context_id,
+    // Add consumer key to requests to indicate which lti app requests are originating from.
+    oauth_consumer_key: state.settings.oauth_consumer_key,
+    ...action.params
+  };
   const promise = api.execRequest(
     action.method,
     action.url,
     state.settings.api_url,
     state.jwt,
     state.settings.csrf_token,
-    action.params,
+    updatedParams,
     action.body,
     action.headers,
     action.timeout);
