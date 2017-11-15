fcf770abe164a2f284bcdd3cbb57452f6f95777f
dittonjs <jditton.atomic@gmail.com>
2017-11-14 18:07:23 -0700

    Refactor communicator to be more generic

--- src/communications/communicator.js
+++ src/communications/communicator.js
@@ -1,6 +1,9 @@
 export default class Communicator {
+  constructor(domain = '*') {
+    this.domain = domain;
+  }
 
-  static enableListener(handler) {
+  enableListener(handler) {
     // Create IE + others compatible event handler
     const eventMethod = window.addEventListener ? 'addEventListener' : 'attachEvent';
     const eventer = window[eventMethod];
@@ -9,15 +12,19 @@ export default class Communicator {
     eventer(this.messageEvent, e => handler.handleComm(e), false);
   }
 
-  static broadcastMsg(payload) {
+  comm(payload) {
+    parent.postMessage(JSON.stringify(payload), this.domain);
+  }
+
+  broadcast(payload) {
     // Post up the entire chain of parent windows.  This supports our use case
     // of the assessment-player being embedded in dumb content which is then
     // embedded in another controller that can understand this message.
     const parents = new Set();
     let p = parent;
     while (!parents.has(p)) {
-      console.log(`Posting message: ${JSON.stringify(payload)}`);
-      p.postMessage(JSON.stringify(payload), '*');
+      // console.log(`Posting message: ${JSON.stringify(payload)}`);
+      p.postMessage(JSON.stringify(payload), this.domain);
       parents.add(p);
       p = p.parent;
     }
---
+++ src/middleware/post_message.spec.js
@@ -0,0 +1,60 @@
+import postMessage, { HandlerSingleton } from './post_message';
+import Helper from '../specs_support/helper';
+
+describe('postMessage middleware', () => {
+  Helper.mockAllAjax();
+
+  beforeEach(() => {
+    HandlerSingleton.instance = null;
+  });
+
+  it('implements Redux middleware interface', () => {
+    const store = { getState: () => {} };
+    const middleware = postMessage(store);
+    const nextHandler = () => {};
+    const actionHandler = middleware(nextHandler);
+
+    expect(postMessage.length).toBe(1);
+    expect(typeof middleware).toBe('function');
+    expect(middleware.length).toBe(1);
+    expect(typeof actionHandler).toBe('function');
+    expect(actionHandler.length).toBe(1);
+  });
+
+  it('calls prepareInstance', () => {
+    const action = {
+      type: 'TEST',
+      postMessage: true
+    };
+
+    const middleware = postMessage(Helper.makeStore());
+    const nextHandler = () => {};
+    const actionHandler = middleware(nextHandler);
+    spyOn(HandlerSingleton, 'prepareInstance');
+    actionHandler(action);
+    expect(HandlerSingleton.prepareInstance).toHaveBeenCalled();
+  });
+
+  it('calls comm when it recieves an action', () => {
+    const action = {
+      type: 'POST_MESSAGE',
+      postMessage: true
+    };
+
+    const middleware = postMessage(Helper.makeStore());
+    const nextHandler = () => {};
+    const actionHandler = middleware(nextHandler);
+    HandlerSingleton.prepareInstance(() => {});
+    spyOn(HandlerSingleton.communicator, 'comm');
+    actionHandler(action);
+    expect(HandlerSingleton.communicator.comm).toHaveBeenCalled();
+  });
+
+  it('HandlerSingleton.handleComm calls the dispatch', () => {
+    const spy = jest.fn();
+    HandlerSingleton.prepareInstance(spy);
+    HandlerSingleton.instance.handleComm({ data: '{}' });
+    expect(spy).toHaveBeenCalled();
+  });
+
+});
