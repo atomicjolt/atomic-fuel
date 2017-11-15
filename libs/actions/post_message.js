9ee149574869c84ac1d840cc715d9b30b5760d01
dittonjs <jditton.atomic@gmail.com>
2017-11-15 11:03:20 -0700

    new build

--- libs/actions/post_message.js
+++ libs/actions/post_message.js
@@ -16,9 +16,11 @@ var actions = ['POST_MESSAGE'];
 var Constants = exports.Constants = (0, _wrapper2.default)(actions, []);
 
 var postMessage = exports.postMessage = function postMessage(message) {
+  var broadcast = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
   return {
     type: Constants.POST_MESSAGE,
     postMessage: true,
+    broadcast: broadcast,
     message: message
   };
 };
