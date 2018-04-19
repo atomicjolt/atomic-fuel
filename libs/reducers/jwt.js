211576684c602db059c3d06bcceb404a2a43567f
Justin Ball <justinball@gmail.com>
2018-04-19 14:14:52 -0600

    Built files

--- libs/reducers/jwt.js
+++ libs/reducers/jwt.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", {
 
 var _jwt = require('../actions/jwt');
 
-var initialState = {};
+var initialState = '';
 
 exports.default = function () {
   var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : initialState;
@@ -15,7 +15,14 @@ exports.default = function () {
   switch (action.type) {
 
     case _jwt.Constants.REFRESH_JWT_DONE:
-      return action.payload.jwt;
+      if (action.payload.jwt) {
+        // Ensure we received a valid jwt. If the server isn't available we
+        // will get undefined. If there is a chance the current jwt is still
+        // valid we want to leave it in place. Note that this typically happens
+        // when the user loses network connectivity.
+        return action.payload.jwt;
+      }
+      return state;
 
     default:
       return state;
