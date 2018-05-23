65c839b1b6bee7f27624b41cfaf966923eb9092a
Justin Ball <justinball@gmail.com>
2018-05-23 13:59:30 -0600

    Export communication functions

--- libs/communications/communicator.js
+++ libs/communications/communicator.js
@@ -5,14 +5,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = undefined;
 
-var _set = require('babel-runtime/core-js/set');
-
-var _set2 = _interopRequireDefault(_set);
-
-var _stringify = require('babel-runtime/core-js/json/stringify');
-
-var _stringify2 = _interopRequireDefault(_stringify);
-
 var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
 
 var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
@@ -21,7 +13,38 @@ var _createClass2 = require('babel-runtime/helpers/createClass');
 
 var _createClass3 = _interopRequireDefault(_createClass2);
 
+var _set = require('babel-runtime/core-js/set');
+
+var _set2 = _interopRequireDefault(_set);
+
+var _stringify = require('babel-runtime/core-js/json/stringify');
+
+var _stringify2 = _interopRequireDefault(_stringify);
+
+exports.postMessage = postMessage;
+exports.broadcastMessage = broadcastMessage;
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// Just post to the parent
+function postMessage(payload) {
+  var domain = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '*';
+
+  parent.postMessage((0, _stringify2.default)(payload), domain);
+}
+
+// Post up the entire chain of parent windows.
+function broadcastMessage(payload) {
+  var domain = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '*';
+
+  var parents = new _set2.default();
+  var p = parent;
+  while (!parents.has(p)) {
+    p.postMessage((0, _stringify2.default)(payload), domain);
+    parents.add(p);
+    p = p.parent;
+  }
+}
 
 var Communicator = function () {
   function Communicator() {
@@ -46,22 +69,12 @@ var Communicator = function () {
   }, {
     key: 'comm',
     value: function comm(payload) {
-      parent.postMessage((0, _stringify2.default)(payload), this.domain);
+      postMessage(payload, this.domain);
     }
   }, {
     key: 'broadcast',
     value: function broadcast(payload) {
-      // Post up the entire chain of parent windows.  This supports our use case
-      // of the assessment-player being embedded in dumb content which is then
-      // embedded in another controller that can understand this message.
-      var parents = new _set2.default();
-      var p = parent;
-      while (!parents.has(p)) {
-        // console.log(`Posting message: ${JSON.stringify(payload)}`);
-        p.postMessage((0, _stringify2.default)(payload), this.domain);
-        parents.add(p);
-        p = p.parent;
-      }
+      broadcastMessage(payload, this.domain);
     }
   }]);
   return Communicator;
--- src/communications/communicator.js
+++ src/communications/communicator.js
@@ -1,3 +1,19 @@
+// Just post to the parent
+export function postMessage(payload, domain = '*') {
+  parent.postMessage(JSON.stringify(payload), domain);
+}
+
+// Post up the entire chain of parent windows.
+export function broadcastMessage(payload, domain = '*') {
+  const parents = new Set();
+  let p = parent;
+  while (!parents.has(p)) {
+    p.postMessage(JSON.stringify(payload), domain);
+    parents.add(p);
+    p = p.parent;
+  }
+}
+
 export default class Communicator {
   constructor(domain = '*') {
     this.domain = domain;
@@ -13,21 +29,11 @@ export default class Communicator {
   }
 
   comm(payload) {
-    parent.postMessage(JSON.stringify(payload), this.domain);
+    postMessage(payload, this.domain);
   }
 
   broadcast(payload) {
-    // Post up the entire chain of parent windows.  This supports our use case
-    // of the assessment-player being embedded in dumb content which is then
-    // embedded in another controller that can understand this message.
-    const parents = new Set();
-    let p = parent;
-    while (!parents.has(p)) {
-      // console.log(`Posting message: ${JSON.stringify(payload)}`);
-      p.postMessage(JSON.stringify(payload), this.domain);
-      parents.add(p);
-      p = p.parent;
-    }
+    broadcastMessage(payload, this.domain);
   }
 
 }
