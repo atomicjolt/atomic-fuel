29d11c5fbc0c4c3cd5a401282b9608351725082a
Justin Ball <justinball@gmail.com>
2018-08-29 23:02:02 -0600

    Fix insert style tag

--- libs/libs/styles.js
+++ libs/libs/styles.js
@@ -6,9 +6,12 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = addStyles;
 function addStyles(styles) {
   var id = 'atomic-fuel-styles';
-  var styleEl = document.getElementById(id) || document.createElement('style');
-  styleEl.id = id;
-  document.head.appendChild(styleEl);
+  var styleEl = document.getElementById(id);
+  if (!styleEl) {
+    styleEl = document.createElement('style');
+    styleEl.id = id;
+    document.head.appendChild(styleEl);
+  }
   var styleSheet = styleEl.sheet;
   styleSheet.insertRule(styles, styleSheet.cssRules.length);
 }
--- src/libs/styles.js
+++ src/libs/styles.js
@@ -1,8 +1,11 @@
 export default function addStyles(styles) {
   const id = 'atomic-fuel-styles';
-  const styleEl = document.getElementById(id) || document.createElement('style');
-  styleEl.id = id;
-  document.head.appendChild(styleEl);
+  let styleEl = document.getElementById(id);
+  if (!styleEl) {
+    styleEl = document.createElement('style');
+    styleEl.id = id;
+    document.head.appendChild(styleEl);
+  }
   const styleSheet = styleEl.sheet;
   styleSheet.insertRule(styles, styleSheet.cssRules.length);
 }
