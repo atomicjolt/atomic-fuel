b4fb0f6f0d650ba734d049de007eca79d9c980c4
Justin Ball <justinball@gmail.com>
2019-01-17 15:50:53 -0700

    Added class to support JWT refresh and decoding

--- src/loaders/jwt.js
+++ src/loaders/jwt.js
@@ -1,8 +1,56 @@
 import { refreshJwt } from '../actions/jwt';
+import api from '../api/api';
+
+const REFRESH = 1000 * 60 * 60 * 23; // every 23 hours
 
 export default function(dispatch, currentUserId) {
-  const refresh = 1000 * 60 * 20; // every 20 minutes
   setInterval(() => {
     dispatch(refreshJwt(currentUserId));
-  }, refresh);
+  }, REFRESH);
+}
+
+export class Jwt {
+  constructor(jwt, apiUrl, oauthConsumerKey = null) {
+    this.jwt = jwt;
+    this.apiUrl = apiUrl;
+
+    this.oauthConsumerKey = oauthConsumerKey;
+
+    const base64Url = this.jwt.split('.')[1];
+    const base64 = base64Url.replace('-', '+').replace('_', '/');
+    this._decodedJwt = JSON.parse(window.atob(base64));
+
+    this.userId = this._decodedJwt.user_id;
+    this.contextId = this._decodedJwt.context_id;
+    this.oauthConsumerKey = this._decodedJwt.kid || oauthConsumerKey;
+  }
+
+  enableRefresh() {
+    const url = `api/jwts/${this.userId}`;
+    const params = {
+      // Add the context id from the lti launch
+      context_id: this.contextId,
+      // Add consumer key to requests to indicate which lti app requests are originating from.
+      oauth_consumer_key: this.oauthConsumerKey,
+    };
+    setInterval(() => {
+      api.get(url, this.apiUrl, this.jwt, null, params, null).then((response) => {
+        this.jwt = response.body.jwt;
+      });
+    }, REFRESH);
+  }
+
+  get currentJwt() {
+    return this.jwt;
+  }
+
+  get decodedJwt() {
+    return this._decodedJwt;
+  }
+
+  get isjwtExpired() {
+    // Rails does seconds since the epoch instead of milliseconds so we multiple by 1000
+    return this.decodedJwt.exp * 1000 < Date.now();
+  }
+
 }
