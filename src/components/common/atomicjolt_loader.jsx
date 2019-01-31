d06976bddcf9d5e1ad418fa3b4b88980219e4566
ThinkBrandonThink <brandonfindlay90@gmail.com>
2019-01-30 17:14:39 -0800

    loader style changes

--- src/components/common/atomicjolt_loader.jsx
+++ src/components/common/atomicjolt_loader.jsx
@@ -4,32 +4,37 @@ import addStyles from '../../libs/styles';
 
 function renderStyles() {
   addStyles(`.atomicjolt-loading-animation {
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: -webkit-flex;
     display: flex;
-    -webkit-align-items: center;
     align-items: center;
+    justify-content: center;
     flex-direction: column;
-    margin: 10rem;
+    margin: 48px auto;
+    width: 72px;
+    height: 72px;
+    border-radius: 50%;
+    background-image: linear-gradient(to top right, #B388FF, #E7B9FF);
+    box-shadow: -2px 3px 6px rgba(0,0,0,0.25);
   }`);
   addStyles(`.atomicjolt-loading-animation svg {
-    width: 80px;
+    width: 38px;
+    position: relative;
+    left: -2px;
+    top: -1px;
   }`);
   addStyles(`.atomicjolt-loading-animation svg polygon, .atomicjolt-loading-animation svg polyline {
     fill: none;
-    stroke: #333;
+    stroke: #fff;
     stroke-linecap: round;
     stroke-linejoin: round;
-    stroke-width: 6px;
+    stroke-width: 8px;
   }`);
   addStyles(`.atomicjolt-loading-animation svg .cls-1 {
     stroke-dasharray: 0 250;
-    animation: line1 1.5s infinite ease;
+    animation: line1 1.5s infinite cubic-bezier(0.455, 0.03, 0.515, 0.955);
   }`);
   addStyles(`.atomicjolt-loading-animation svg .cls-2 {
     stroke-dasharray: 0 270;
-    animation: line2 1.5s infinite ease;
+    animation: line2 1.5s infinite cubic-bezier(0.455, 0.03, 0.515, 0.955);
   }`);
   addStyles(`@keyframes line1 {
     0% {
@@ -66,15 +71,13 @@ export default class Loader extends React.PureComponent {
   render() {
     renderStyles();
     return (
-      <div>
-        <div className="atomicjolt-loading-animation">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 91.87 114.09" role="img" aria-label="loading">
-            <g data-name="Layer 2">
-              <polygon className="cls-1" points="40.45 111.32 89.11 99.26 71.35 19.9 21.1 89.71 40.45 111.32" />
-              <polyline className="cls-2" points="50.67 2.77 2.77 69.96 25.47 94.65 66.36 84.13 50.67 2.77 71.35 19.9" />
-            </g>
-          </svg>
-        </div>
+      <div className="atomicjolt-loading-animation">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 91.87 114.09" role="img" aria-label="loading">
+          <g data-name="Layer 2">
+            <polygon className="cls-1" points="40.45 111.32 89.11 99.26 71.35 19.9 21.1 89.71 40.45 111.32" />
+            <polyline className="cls-2" points="50.67 2.77 2.77 69.96 25.47 94.65 66.36 84.13 50.67 2.77 71.35 19.9" />
+          </g>
+        </svg>
       </div>
     );
   }
