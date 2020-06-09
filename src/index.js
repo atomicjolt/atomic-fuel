382afbf28d48ac7272296c8ba72374be566027cb
Ryker Blunck <rykerblunck@gmail.com>
2020-06-09 09:54:24 -0600

    add a global exports file so things can easily by imported without looking up full paths

---
+++ src/index.js
@@ -0,0 +1,77 @@
+// REDUX ACTIONS
+export {
+  addError as addErrorAction,
+  clearErrors as clearErrorsAction,
+  Constants as ErrorConstants
+} from './actions/errors';
+export {
+  refreshJwt as refreshJwtAction,
+  Constants as JwtConstants
+} from './actions/jwt';
+export {
+  openModal as openModalAction,
+  closeModal as closeModalAction,
+  Constants as ModalConstants
+} from './actions/modal';
+export {
+  postMessage as postMessageAction,
+  Constants as PostMessageConstants
+} from './actions/post_message';
+
+// REDUX REDUCERS
+export { default as errorReducer } from './reducers/errors';
+export { default as jwtReducer } from './reducers/jwt';
+export { default as modalReducer } from './reducers/modal';
+export {
+  default as settingsReducer,
+  getInitialSettings,
+} from './reducers/settings';
+
+// REDUX MIDDLEWARE
+export { default as postMessageMiddleware } from './middleware/post_message';
+export {
+  apiRequest,
+  default as ApiMiddleware
+} from './middleware/api';
+
+// REDUX LOADERS
+export { default as jwtLoader } from './loaders/jwt';
+
+// REDUX STORE
+export { default as configureStore } from './store/configure_store';
+
+// API
+export { default as Api } from './api/api';
+export {
+  default as Communicator,
+  postMessage,
+  broadcastRawMessage,
+  broadcastMessage,
+} from './communications/communicator';
+
+// REACT COMPONENTS
+export {
+  SettingsContext,
+  withSettings
+} from './components/settings';
+export {
+  default as AtomicjoltLoader,
+  Loader as AtomicjoltLoaderRaw
+} from './components/common/atomicjolt_loader';
+export { default as GqlStatus } from './components/common/gql_status';
+export { default as IframeResizeWrapper } from './components/common/resize_wrapper';
+export { default as InlineError } from './components/common/errors/inline_error';
+
+// APOLLO REACT COMPONENTS
+export { default as AtomicMutation } from './graphql/atomic_mutation';
+export { default as AtomicQuery } from './graphql/atomic_query';
+
+// DECORATORS
+export { default as modalDecorator } from './decorators/modal';
+
+// LIBS
+export { default as iframeResizeHandler } from './libs/resize_iframe';
+export {
+  isLtiInstructor,
+  isLtiAdmin
+} from './libs/lti_roles';
