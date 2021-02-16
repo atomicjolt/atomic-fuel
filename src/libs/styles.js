8bd2ea0795a7c767402c5afb06d14bcfc1179589
momotofu <mrkita.reece@gmail.com>
2021-02-16 12:36:01 +0900

    Added performance enhancements to AddStyles function

--- src/libs/styles.js
+++ src/libs/styles.js
@@ -1,11 +1,23 @@
-export default function addStyles(styles) {
-  const id = 'atomic-fuel-styles';
+const function getAddStyles() {
   let styleEl = document.getElementById(id);
-  if (!styleEl) {
-    styleEl = document.createElement('style');
-    styleEl.id = id;
-    document.head.appendChild(styleEl);
-  }
-  const styleSheet = styleEl.sheet;
-  styleSheet.insertRule(styles, styleSheet.cssRules.length);
+  const memo = {};
+  const id = 'atomic-fuel-styles';
+
+  return (styles) => {
+    if (!styleEl) {
+      styleEl = document.createElement('style');
+      styleEl.id = id;
+      document.head.appendChild(styleEl);
+    }
+
+    const classes = styles.match(/.*[^{]/)[0];
+
+    if (memo[classes] === undefined) {
+      styleEl.sheet.insertRule(styles, styleSheet.cssRules.length);
+    } else {
+      memo[classes] = 1;
+    }
+  };
 }
+
+export getAddStyles();
