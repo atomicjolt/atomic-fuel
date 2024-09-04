bd4671153a81abd1b2cb7d6241d7048a14f7026a
Ryker Blunck <ryker.blunck@atomicjolt.com>
2024-09-04 13:56:25 -0600

    restore the GQL status component, remove 'prepare' script because we don't want that to run when installing. Remove some bad imports

---
+++ libs/components/GqlStatus/index.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = __importDefault(require("react"));
+var prop_types_1 = __importDefault(require("prop-types"));
+var atomicjolt_loader_1 = __importDefault(require("../common/atomicjolt_loader"));
+var inline_error_1 = __importDefault(require("../common/errors/inline_error"));
+function GqlStatus(props) {
+    if (props.loading) {
+        return (react_1.default.createElement(atomicjolt_loader_1.default, { message: props.loadMessage }));
+    }
+    if (props.error) {
+        return react_1.default.createElement(inline_error_1.default, { error: props.error.message });
+    }
+    return null;
+}
+exports.default = GqlStatus;
+GqlStatus.propTypes = {
+    loading: prop_types_1.default.bool,
+    error: prop_types_1.default.shape({ message: prop_types_1.default.string }),
+    loadMessage: prop_types_1.default.string,
+};
+//# sourceMappingURL=index.js.map
